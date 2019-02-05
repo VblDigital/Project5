36a7018d943857dc6ab2940951d9799ca537129a
vbopenclass <vbopenclass@gmail.com>
2019-02-05 18:26:52 +0100

    setup(login form) Created the form to login to administration tool

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VALEBLES
+ * Date: 24/01/2019
+ * Time: 17:30
+ */
+//connection to the database
+require_once('include/config.php');
+?>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            include 'connect_form.php';
+        ?>
+    </body>
+</html>
---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VALEBLES
+ * Date: 05/02/2019
+ * Time: 17:57
+ */
+
+session_start();
+?>
+
+<html>
+<head>
+</head>
+<body>
+<p>Bienvenue <?php echo '$login' ?></p>
+</body>
+</html>
