333b1f3f38335e8ce43e424a4c49ef45707ecd08
vbopenclass <vbopenclass@gmail.com>
2019-03-04 20:33:09 +0100

    issue(class) Problem witĥ the link between parent and child class

--- mesClass/Database.php
+++ mesClass/Database.php
@@ -38,29 +38,5 @@ class Database
         return $pdo;
     }
 
-    public function query($stmt, $class_name) {
-        $req = $this->getPDO()->query($stmt);
-        $result = $req -> fetchAll(PDO::FETCH_CLASS, $class_name);
-        return $result;
-    }
 
-    public function prepare_username($statement, $class_name) {
-        $req = $this->pdo->prepare($statement);
-        $req->execute(array());
-        $req->setFetchMode(PDO::FETCH_CLASS, $class_name);
-        $datas = $req->fetch();
-        return $datas;
-    }
-
-    public function prepare($statement, $attributes, $class_name, $one = false) {
-        $req = $this->getPDO()->prepare($statement);
-        $req->execute($attributes);
-        $req->setFetchMode(PDO::FETCH_CLASS, $class_name);
-        if ($one) {
-            $datas = $req->fetch();
-        } else {
-            $datas = $req->fetchAll();
-        }
-        return $datas;
-    }
 }
--- post/postlist.php
+++ post/postlist.php
@@ -1,52 +1,86 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: VALEBLES
- * Date: 26/02/2019
- * Time: 17:01
- */
-//to create the objects 'Post'
-?>
-<html>
-<head>
-</head>
-<body>
-    <div style="text-align: center"><h1>Liste des billets</h1></div><br/>
-    <!-- Generate all the posts in a variable -->
-    <div>
-        <?php foreach ($bdd->query('SELECT * FROM post', mesClass\Post::class) as $post): ;
-        $id=$post->getCreatedBy();
-        $sth = $bdd->prepare_username('SELECT username FROM user WHERE id = ' . $id, 'mesClass\User');
-        ?>
-    <!-- Display the posts -->
-    <h2>
-        <?php echo $post->getTitle(); ?>
-    </h2>
-    <div>
-        Publié le <?php echo $post->getCreatedDate(); ?> par <?php echo $sth->getUsername(); ?>
+<div class="row">
+    <div class="col-md-12 blog-main">
+        <div class="blog-post">
+            <!-- Generate all the posts in a variable -->
+            <div>
+                <?php
+                foreach ($bdd->query('SELECT * FROM post', mesClass\Functions::class) as $post): ;
+                $id=$post->getId();
+                //$sth2 = $bdd->prepare_category(SELECT )
+                $user=$post->getCreatedBy();
+                $sth = $bdd->prepare_reference('SELECT username FROM user WHERE id = ' . $user, 'mesClass\User');
+                ?>
+                <!-- Display the posts -->
+                <h2 class="blog-post-title">
+                    <?php echo $post->getTitle(); ?>
+                </h2>
+                <p class="blog-post-meta">Publié le <?php $date = $post->getCreatedDate(); $newdate = date("d-m-Y", strtotime($date)); echo $newdate; ?> par
+                <?php echo $sth->getUsername(); ?>
+                </p>
+                <div style="max-width: 900px">
+                    <?php
+                    //create the small extract of post
+                    echo $post->getChapo(); ?><br />
+                </div>
+                <div>
+                <!-- Link to view all the post -->
+                <a href="
+                    <?php
+                    $id_url=$post->getId();
+                    echo 'index.php?p=allpost&id=' . $id_url
+                    ?>
+                ">Lire la suite</a>
+                </div>
+                <p class="blog-post-meta">
+                    <?php
+                    if($post->getUpdatedDate()!="") {
+                        $update = $post->getUpdatedDate();
+                        $newdupdate = date("d-m-Y", strtotime($update));
+                        echo 'Modifié le ' . $newdupdate;
+                    }
+                    ?>
+                </p>
+            </div>
+            <hr>
+            <?php endforeach; ?>
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+            </nav>
+        </div>
+        <!--<aside class="col-md-4 blog-sidebar">
+            <div class="p-4 mb-3 bg-light rounded">
+                <h4 class="font-italic">About</h4>
+                <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            </div>
+            <div class="p-4">
+                <h4 class="font-italic">Archives</h4>
+                <ol class="list-unstyled mb-0">
+                    <li><a href="#">March 2014</a></li>
+                    <li><a href="#">February 2014</a></li>
+                    <li><a href="#">January 2014</a></li>
+                    <li><a href="#">December 2013</a></li>
+                    <li><a href="#">November 2013</a></li>
+                    <li><a href="#">October 2013</a></li>
+                    <li><a href="#">September 2013</a></li>
+                    <li><a href="#">August 2013</a></li>
+                    <li><a href="#">July 2013</a></li>
+                    <li><a href="#">June 2013</a></li>
+                    <li><a href="#">May 2013</a></li>
+                    <li><a href="#">April 2013</a></li>
+                </ol>
+            </div>
+            <div class="p-4">
+                <h4 class="font-italic">Elsewhere</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ol>
+            </div>
+        </aside><!-- /.blog-sidebar -->
+
+        <!-- /.row -->
     </div>
-    <div style="max-width: 900px">
-        <?php
-        //create the small extract of post
-        echo $post->getChapo(); ?><br />
-    </div>
-    <div>
-        <!-- Link to view all the post -->
-        <a href="
-            <?php
-                $id_url=$post->getId();
-                echo 'index.php?p=allpost&id=' . $id_url ?>">Lire la suite</a><br/><br/>
-    </div>
-    <div><b>
-        <?php
-        if($post->getUpdatedDate()!="") {
-            echo 'Modifié le ' . $post->getUpdatedDate();
-        }
-        ?>
-    </b></div>
-    <br/>
-    <!-- To separate the posts -->
-    __________________________________________________________________________________________________________
-    <?php endforeach; ?>
-</body>
-</html>
+</div>
+
